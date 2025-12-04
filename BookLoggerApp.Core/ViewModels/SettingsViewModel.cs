77ebdfc3b54b65f7ea0349cbf2256765e7ae55f3
Ben Sowieja <ben.sowieja@4sellers.de>
2025-12-04 08:24:10 +0100

    Made _appVersion readonly

--- BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
+++ BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
@@ -18,7 +18,7 @@ public partial class SettingsViewModel : ViewModelBase
     private AppSettings _settings = new();
 
     [ObservableProperty]
-    private string _appVersion = "0.3.19";
+    private readonly string _appVersion = "0.3.19";
 
     [RelayCommand]
     public async Task LoadAsync()
