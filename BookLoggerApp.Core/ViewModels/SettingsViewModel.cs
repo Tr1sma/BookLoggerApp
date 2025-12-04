d6cf7b8d33ee5293a54a42a0929d1f29fa027192
Tristan <tristan.atze@gmail.com>
2025-12-04 08:31:28 +0100

    Bumped version to 0.4.0

--- BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
+++ BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
@@ -18,7 +18,7 @@ public partial class SettingsViewModel : ViewModelBase
     private AppSettings _settings = new();
 
     [ObservableProperty]
-    private string _appVersion = "0.3.19";
+    private string _appVersion = "0.4.0";
 
     [RelayCommand]
     public async Task LoadAsync()
