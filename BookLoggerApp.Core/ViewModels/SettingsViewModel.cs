c46b7b1e6c445722c61577a518138401bb992434
Tristan <tristan.atze@gmail.com>
2026-04-01 19:54:48 +0200

    Updated settings view model

--- BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
+++ BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
@@ -40,7 +40,7 @@ public partial class SettingsViewModel : ViewModelBase
     private AppSettings _settings = new();
 
     [ObservableProperty]
-    private string _appVersion = "0.7.4";
+    private string _appVersion = "0.7.5";
 
     [ObservableProperty]
     private string _migrationLog;
