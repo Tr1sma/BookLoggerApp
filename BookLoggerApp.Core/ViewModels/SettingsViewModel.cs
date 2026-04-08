58c58c5c46e7381ac31ea3cf55292c2d7bbadae6
Tristan <tristan.atze@gmail.com>
2026-04-08 11:47:00 +0200

    Fixed Android launcher icon with adaptive mipmap resources

--- BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
+++ BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
@@ -44,7 +44,7 @@ public partial class SettingsViewModel : ViewModelBase
     private AppSettings _settings = new();
 
     [ObservableProperty]
-    private string _appVersion = "0.8.2";
+    private string _appVersion = "0.8.3";
 
     [ObservableProperty]
     private string _migrationLog;
