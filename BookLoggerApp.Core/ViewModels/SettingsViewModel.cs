35c8ff389a9d04298cc9fc49148ec5c4f1397123
Tristan <tristan.atze@gmail.com>
2026-04-12 22:26:49 +0200

    Bumped version to 0.9.2

--- BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
+++ BookLoggerApp.Core/ViewModels/SettingsViewModel.cs
@@ -45,7 +45,7 @@ public partial class SettingsViewModel : ViewModelBase
     private AppSettings _settings = new();
 
     [ObservableProperty]
-    private string _appVersion = "0.9.1";
+    private string _appVersion = "0.9.2";
 
     [ObservableProperty]
     private string _migrationLog;
